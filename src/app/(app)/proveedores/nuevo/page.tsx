3925741b0919d738200586eea4cedd909d428db1
Viviana Díaz <vizaddd@gmail.com>
2025-11-03 18:02:49 +0000

    en /proveedores/nuevo agrega un botón para retroceder

--- src/app/(app)/proveedores/nuevo/page.tsx
+++ src/app/(app)/proveedores/nuevo/page.tsx
@@ -1,3 +1,8 @@
+
+'use client';
+
+import Link from 'next/link';
+import { ChevronLeft } from 'lucide-react';
 import { Header } from '@/components/header';
 import { AddSupplierForm } from '@/components/proveedores/add-supplier-form';
 import {
@@ -7,12 +12,20 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 export default function NuevoProveedorPage() {
   return (
     <>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-         <h1 className="text-2xl font-bold">Agregar Nuevo Proveedor</h1>
+        <div className="flex items-center gap-4">
+          <Button variant="outline" size="icon" asChild>
+            <Link href="/proveedores">
+              <ChevronLeft className="h-4 w-4" />
+            </Link>
+          </Button>
+          <h1 className="text-2xl font-bold">Agregar Nuevo Proveedor</h1>
+        </div>
         <Card>
           <CardHeader>
             <CardTitle>Información del Proveedor</CardTitle>
